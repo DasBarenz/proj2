48558aa706dbadd8cbdf92165c05ff4cba5032fc
kkopp <kristinakopp@Kristinas-MacBook-Pro.local>
2019-01-26 14:00:10 -0700

    made updates to routing, adding database info

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,25 +1,46 @@
-var db = require("../models");
+var db = require ('../models');
 
-module.exports = function(app) {
+module.exports = function (app) {
+  // Get all user data
+  app.get ('/api/userdata', function (req, res) {
+    db.UserInfo.findAll ({}).then (function (dbUserInfos) {
+      res.json (dbUserInfos);
+    });
+  });
 
   // Get all user data
-  app.get("/api/userdata", function(req, res) {
-    db.UserInfo.findAll({}).then(function(dbUserInfos) {
-      res.json(dbUserInfos);
+  app.get ('/api/zipcode', function (req, res) {
+    db.ZipCode.findAll ({}).then (function (dbZipCodes) {
+      res.json (dbZipCodes);
     });
   });
 
   // Create a new example
-  app.post("/api/userdata", function(req, res) {
-    db.UserInfo.create(req.body).then(function(dbUserInfos) {
-      res.json(dbUserInfos);
-    });
+  app.post ('/api/userdata', function (req, res) {
+    db.UserInfo
+      .create ({
+        name: req.body.name,
+        zipcode: req.body.zipcode,
+      })
+      .then (function (dbUserInfos) {
+        res.json (dbUserInfos);
+      });
+  });
+
+  app.post ('/api/userdata', function (req, res) {
+    db.UserInfo
+      .update ({score: req.body.score}, {where: {id: req.body.id}})
+      .then (function (dbUserInfos) {
+        res.json (dbUserInfos);
+      });
   });
 
   // Delete an example by id
-  app.delete("/api/userdata/:id", function(req, res) {
-    db.UserInfo.destroy({ where: { id: req.params.id } }).then(function(dbUserInfos) {
-      res.json(dbUserInfos);
-    });
+  app.delete ('/api/userdata/:id', function (req, res) {
+    db.UserInfo
+      .destroy ({where: {id: req.params.id}})
+      .then (function (dbUserInfos) {
+        res.json (dbUserInfos);
+      });
   });
 };
